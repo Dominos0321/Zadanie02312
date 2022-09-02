25177edfb107afca2816d2ef0b76c8d4d630df1d
sztoslol <kubuszeski05@wp.pl>
2022-09-02 15:54:11 +0200

    part2

--- app/src/main/java/com/example/zadanie/MainActivity.kt
+++ app/src/main/java/com/example/zadanie/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.zadanie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val widok = findViewById<Button>(R.id.button);
+        val obraz = findViewById<ImageView>(R.id.imageView);
+        val tekst = findViewById<TextView>(R.id.textView);
+
+        var x =0;
+
+        widok.setOnClickListener {
+
+            if(x==0) {
+                obraz.visibility= View.VISIBLE;
+                tekst.visibility= View.VISIBLE;
+                x++;
+            }
+
+            else if(x==1) {
+                obraz.visibility= View.INVISIBLE;
+                tekst.visibility= View.INVISIBLE;
+                x--;
+            }
+        }
+
+
+
     }
 }
